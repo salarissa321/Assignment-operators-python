17d4a2d443f4d28b79e36964c5286a6269f78e18
salarissa321 <salarissa321@gmail.com>
2023-05-02 13:15:10 +0200

    Assignment-Operators-python

---
+++ Assignment-operators-python.py
@@ -0,0 +1,61 @@
+
+
+
+
+#-----------------------------------
+#------Assignment Operators------
+#-----------------------------------
+# =
+# +=
+# -=
+# %=
+# *=
+# /=
+# **=
+# //==
+#-------------------------------------
+
+
+x = 10 # Var One
+y = 20 # Var Two
+# Var One = Self [Operator +] Var Two
+x = x + y # 30
+# Var One [Operator +]= Var Two
+x += y # 30  # x = x + y
+z = x + y # 30
+print(x)
+
+print("-" * 50) # --------------------------------------------------
+
+x = 10 # Var One
+y = 20 # Var Two
+# Var One = Self [Operator -] Var Two
+x = x - y # -10
+# Var One [Operator -]= Var Two
+x -= y # -10  # x = x - y
+print(x) 
+
+print("-" * 50) # --------------------------------------------------
+
+# x = 10 # Var One
+# y = 20 # Var Two
+# # Var One = Self [Operator *] Var Two
+x = x * y # 200
+# # Var One [Operator *]= Var Two
+# x *= y  # 200 x = x * y
+print(x)
+
+
+print("-" * 50) # --------------------------------------------------
+
+x = 10 # Var One
+y = 20 # Var Two
+# Var One = Self [Operator *] Var Two
+x = x / y # 0.5
+# # Var One [Operator /]= Var Two
+x /= y # 0.5  # x = x / y
+print(x) 
+
+print("-" * 50) # --------------------------------------------------
+
+
